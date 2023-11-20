bb94a60ce89e49de1ed626591389458cf0788973
Quon-Lappy <quontex@hotmail.com>
2023-11-20 14:43:59 +1000

    Moved the bot to a class.

--- AutoBradBot.py
+++ AutoBradBot.py
@@ -1,38 +1,113 @@
 import discord
+import asyncio
+
 from discord.ext import tasks
+from WarningManager import Warning, WarningManager
+from datetime import datetime, timezone
 
 class AutoBrad(discord.Client):
 
-    def __init__(self, *, intents: discord.Intents, **options) -> None:
+    def __init__(self, warning_manager: WarningManager, *, intents: discord.Intents, **options) -> None:
         super().__init__(intents=intents, **options)
 
-    async def on_ready(self) -> None:
+        self.thunderstorm_channel: discord.TextChannel = None
+        self.tornado_channel: discord.TextChannel = None
+        self.flood_channel: discord.TextChannel = None
 
-        thunderstorm_channel: discord.TextChannel
-        tornado_channel: discord.TextChannel
-        flood_channel: discord.TextChannel
+        self.current_messages: set[discord.Message] = set()
+
+        self.warning_manager = warning_manager
+
+    async def on_ready(self) -> None:
 
         await self.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name="Sniffing for warnings"))
 
         for channel in self.get_all_channels():
             if channel.type == discord.ChannelType.text:
                 if channel.name == "autobrad-thunderstorm":
-                    thunderstorm_channel = channel
+                    self.thunderstorm_channel = channel
+
                 elif channel.name == "autobrad-tornado":
-                    tornado_channel = channel
+                    self.tornado_channel = channel
+
                 elif channel.name == "autobrad-flood":
-                    flood_channel = channel
+                    self.flood_channel = channel
 
-        await thunderstorm_channel.purge()
-        await tornado_channel.purge()
-        await flood_channel.purge()
+        await self.thunderstorm_channel.purge()
+        await self.tornado_channel.purge()
+        await self.flood_channel.purge()
 
-    tasks.loop(minutes=3)
+        await self.main_loop.start()
+
+    @tasks.loop(minutes=3)
     async def main_loop(self) -> None:
-        pass
 
-    async def format_warnings(self, warning_type: str) -> None:
-        pass
+        i = 0
 
-    async def publish_warnings(self, warning_type: str) -> None:
-        pass
+        for warn in self.warning_manager.warning_set:
+
+            if i == 5:
+                await asyncio.sleep(1)
+
+            message: discord.Embed = self.generate_warning_embed(warn)
+            expiration_time: float = (warn.expiration_time - datetime.now(timezone.utc)).total_seconds()
+
+            if "tornado" in warn.event_type.lower():
+                await self.publish_warning(message, self.thunderstorm_channel, expiration_time)
+
+            elif "thunderstorm" in warn.event_type.lower():
+                await self.publish_warning(message, self.thunderstorm_channel, expiration_time)
+
+            elif "flood" in warn.event_type.lower():
+                await self.publish_warning(message, self.flood_channel, expiration_time)
+
+            i += 1
+
+    def generate_warning_embed(self, warning: Warning) -> discord.Embed:
+        
+        storm_info = [
+        f"**Areas Listed:**",
+        f"**Description:**",
+        f"**Wind Gusts and Source:**",
+        f"**Hail Size and Source:**",
+        f"**Tornado Threat:**",
+        f"Expires <t:{int(warning.expiration_time.timestamp())}:R>",
+        ]
+
+        message_content: list[str] = []
+
+        for line, i in enumerate(storm_info, 0):
+
+            if i == 0:
+                message_content.append(f"{line} {warning.area_desc}")
+
+            elif i == 1:
+                message_content.append(f"{line} {warning.description}")
+            
+            if any(word in warning.event_type.lower() for word in ["tornado", "thunderstorm"]):
+                if i == 2:
+                    message_content.append(f"{line} {warning.wind_gust}/{warning.wind_threat}")
+
+                elif i == 3:
+                    message_content.append(f"{line} {warning.hail_size}/{warning.hail_threat}")
+
+        content_string = '\n'.join(message_content)
+
+        if "tornado" in warning.event_type.lower():
+            embed_colour = discord.Color.red
+
+        elif "thunderstorm" in warning.event_type.lower():
+            embed_colour = discord.Color.yellow
+
+        elif "flood" in warning.event_type.lower():
+            embed_colour = discord.Color.green
+
+        else:
+            print(f"Unsupported Warning Type: {warning.event_type}")
+            embed_colour = discord.Color.dark_grey
+
+        return discord.Embed(colour=embed_colour, title=warning.headline, description=content_string)
+
+    async def publish_warning(self, warning_message: discord.Embed, warning_channel: discord.TextChannel, expiration_time: float) -> dict:
+        
+        message = await warning_channel.send(warning_message)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import WarningManager
 import Nyon_Util
-
 import os
 import logging
+
+from AutoBradBot import AutoBrad
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 
@@ -18,95 +19,95 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 wm = WarningManager.WarningManager(NWS_API)
 
 intents = discord.Intents(messages=True, guilds=True)
-client = discord.Client(intents=intents)
 
-logging.basicConfig(level=logging.INFO)
-discord_logger = logging.getLogger("discord")
-discord_logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
-discord_logger.addHandler(handler)
+AB = AutoBrad(wm, intents=intents)
+AB.run(token=TOKEN)
 
-@tasks.loop(minutes=1)
-async def main_loop(thunderstorm_channel: discord.TextChannel, tornado_channel: discord.TextChannel, flood_channel: discord.TextChannel):
+# logging.basicConfig(level=logging.INFO)
+# discord_logger = logging.getLogger("discord")
+# discord_logger.setLevel(logging.DEBUG)
+# handler = logging.StreamHandler()
+# handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+# discord_logger.addHandler(handler)
 
-    warning_counts = {}
-    sent_warnings = set()
+# @tasks.loop(minutes=1)
+# async def main_loop(thunderstorm_channel: discord.TextChannel, tornado_channel: discord.TextChannel, flood_channel: discord.TextChannel):
 
-    logger = logging.getLogger(Nyon_Util.get_custom_logger_name())
+#     warning_counts = {}
+#     sent_warnings = set()
+
+#     logger = logging.getLogger(Nyon_Util.get_custom_logger_name())
     
-    wm.update()
+#     wm.update()
 
-    if len(wm.warning_set) < 1:
-        logger.info(f"No warnings present")
-        return
+#     if len(wm.warning_set) < 1:
+#         logger.info(f"No warnings present")
+#         return
 
-    logger.info(f"Warnings Updated")
-    logger.info(f"Total Warnings: {len(wm.warning_set)}")
+#     logger.info(f"Warnings Updated")
+#     logger.info(f"Total Warnings: {len(wm.warning_set)}")
 
-    await thunderstorm_channel.purge()
-    await tornado_channel.purge()
-    await flood_channel.purge()
+#     # await thunderstorm_channel.purge()
+#     # await tornado_channel.purge()
+#     # await flood_channel.purge()
 
-    for warning in wm.warning_set:
+#     for warning in wm.warning_set:
 
-        storm_info = [
-            f"**Areas Listed:** {warning.area_desc}",
-            f"**Description:** ```{warning.description}```",
-            f"**Wind Gusts and Source:** {warning.wind_gust} / {warning.wind_threat}",
-            f"**Hail Size and Source:** {warning.hail_size} / {warning.hail_threat},",
-            f"**Tornado Threat:** {warning.tornado_detection}",
-            f"Expires <t:{int(warning.expiration_time.timestamp())}:R>",
-            f"-" * 40
-        ]
+#         storm_info = [
+#             f"**Areas Listed:** {warning.area_desc}",
+#             f"**Description:** ```{warning.description}```",
+#             f"**Wind Gusts and Source:** {warning.wind_gust} / {warning.wind_threat}",
+#             f"**Hail Size and Source:** {warning.hail_size} / {warning.hail_threat},",
+#             f"**Tornado Threat:** {warning.tornado_detection}",
+#             f"Expires <t:{int(warning.expiration_time.timestamp())}:R>",
+#             f"-" * 40
+#         ]
 
-        if warning.id not in sent_warnings:
+#         if warning.id not in sent_warnings:
 
-            sent_warnings.add(warning.id)
+#             sent_warnings.add(warning.id)
 
-            if "tornado" in warning.event_type.lower():
-                warning_embed = discord.Embed(color=discord.Color.red(), title=warning.headline, description="\n".join(storm_info))
-                await tornado_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
-            if "thunderstorm" in warning.event_type.lower():
-                warning_embed = discord.Embed(color=discord.Color.yellow(), title=warning.headline, description="\n".join(storm_info))
-                await thunderstorm_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
-            if "flood" in warning.event_type.lower():
-                warning_embed = discord.Embed(color=discord.Color.green(), title=warning.headline, description="\n".join(storm_info))
-                await flood_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
+#             if "tornado" in warning.event_type.lower():
+#                 warning_embed = discord.Embed(color=discord.Color.red(), title=warning.headline, description="\n".join(storm_info))
+#                 await tornado_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
+#             if "thunderstorm" in warning.event_type.lower():
+#                 warning_embed = discord.Embed(color=discord.Color.yellow(), title=warning.headline, description="\n".join(storm_info))
+#                 await thunderstorm_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
+#             if "flood" in warning.event_type.lower():
+#                 warning_embed = discord.Embed(color=discord.Color.green(), title=warning.headline, description="\n".join(storm_info))
+#                 await flood_channel.send(embed=warning_embed, delete_after=(warning.expiration_time - datetime.now(timezone.utc)).total_seconds())
 
-        try:
-            warning_counts[warning.event_type] += 1
-        except KeyError:
-            warning_counts[warning.event_type] = 1
+#         try:
+#             warning_counts[warning.event_type] += 1
+#         except KeyError:
+#             warning_counts[warning.event_type] = 1
     
-    for k, v in warning_counts.items():
-        logger.info(f"{k}:{v}")
+#     for k, v in warning_counts.items():
+#         logger.info(f"{k}:{v}")
 
-@client.event
-async def on_ready():
+# @client.event
+# async def on_ready():
 
-    logger = logging.getLogger(Nyon_Util.get_custom_logger_name())
+#     logger = logging.getLogger(Nyon_Util.get_custom_logger_name())
 
-    logger.info(f"Bot loaded in as {client.user}")
-    await client.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name="Sniffing for warnings"))
+#     logger.info(f"Bot loaded in as {client.user}")
+#     await client.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name="Sniffing for warnings"))
 
-    thunderstorm_channel: discord.TextChannel
-    tornado_channel: discord.TextChannel
-    flood_channel: discord.TextChannel
+#     thunderstorm_channel: discord.TextChannel
+#     tornado_channel: discord.TextChannel
+#     flood_channel: discord.TextChannel
 
-    for channel in client.get_all_channels():
-        if channel.type == discord.ChannelType.text:
-            if channel.name == "autobrad-thunderstorm":
-                thunderstorm_channel = channel
-            elif channel.name == "autobrad-tornado":
-                tornado_channel = channel
-            elif channel.name == "autobrad-flood":
-                flood_channel = channel
+#     for channel in client.get_all_channels():
+#         if channel.type == discord.ChannelType.text:
+#             if channel.name == "autobrad-thunderstorm":
+#                 thunderstorm_channel = channel
+#             elif channel.name == "autobrad-tornado":
+#                 tornado_channel = channel
+#             elif channel.name == "autobrad-flood":
+#                 flood_channel = channel
 
-    await thunderstorm_channel.purge()
-    await tornado_channel.purge()
-    await flood_channel.purge()
+#     await thunderstorm_channel.purge()
+#     await tornado_channel.purge()
+#     await flood_channel.purge()
 
-    main_loop.start(thunderstorm_channel, tornado_channel, flood_channel)
-
-client.run(TOKEN)
+#     main_loop.start(thunderstorm_channel, tornado_channel, flood_channel)
